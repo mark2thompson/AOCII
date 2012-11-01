1aa9f1155b35361ba2756cb22d5d2f8e511dd370
Mark Thompson <mark2thompson@gmail.com>
2012-11-01 19:18:56 -0400

    added Calculator project to my AOCII file… also created the interface to mimick the project description

---
+++ week2calc/week2calc/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  week2calc
+//
+//  Created by Mark Thompson on 11/1/12.
+//  Copyright (c) 2012 Mark Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ week2calc/week2calc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  week2calc
+//
+//  Created by Mark Thompson on 11/1/12.
+//  Copyright (c) 2012 Mark Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
