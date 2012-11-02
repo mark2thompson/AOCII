3413a3072f32062edf42d9132ec1202b94ba89da
Mark Thompson <mark2thompson@gmail.com>
2012-11-01 20:15:14 -0400

    created simple calculator and background controls with segment controler

--- week2calc/week2calc/ViewController.h
+++ week2calc/week2calc/ViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+
+@interface ViewController : UIViewController {
+
+    float result;
+    IBOutlet UILabel * calculatorScreen;
+    int currentOperation;
+    float currentNumber;
+    
+}
+-(IBAction)buttonDigitPressed:(id)sender;
+-(IBAction)buttonOperationPressed:(id)sender;
+-(IBAction)cancelOperation;
+-(IBAction)changeBGseg:(id)sender;
+
 
 @end
--- week2calc/week2calc/ViewController.m
+++ week2calc/week2calc/ViewController.m
@@ -13,6 +13,55 @@
 @end
 
 @implementation ViewController
+// this is the action for when a digit is pressed... uses the tags to detirmine the number
+-(IBAction)buttonDigitPressed:(id)sender {
+    currentNumber = currentNumber *10 + (float)[sender tag];
+    calculatorScreen.text = [NSString stringWithFormat:@"%f",currentNumber];
+}
+// this is the action for when an operation button is pressed... determines between equals and plus
+-(IBAction)buttonOperationPressed:(id)sender {
+    if (currentOperation == 0) result = currentNumber;
+    else {
+        switch (currentOperation) {
+            case 1:
+                result = result + currentNumber;
+                break;
+        }
+    }
+    currentNumber = 0;
+    calculatorScreen.text = [NSString stringWithFormat:@"%f", result];
+    if ([sender tag] ==0 ) result=0;
+    currentOperation = [sender tag];
+}
+//this clears all inputs and starts back at 0 
+-(IBAction)cancelOperation{
+    currentNumber = 0;
+    calculatorScreen.text = @"0";
+    currentOperation = 0;
+}
+// this is the BG segment switch action and function
+-(IBAction)changeBGseg:(id)sender{
+    UISegmentedControl *BGController = (UISegmentedControl *)sender;
+    if (BGController !=nil)
+    {
+        int segValue = BGController.selectedSegmentIndex;
+        // this uses the segment controllers index to determine what color I am want for the background
+        switch (segValue){
+            case 0:
+                self.view.backgroundColor = [UIColor whiteColor];
+                break;
+                
+            case 1:
+                self.view.backgroundColor = [UIColor blueColor];
+                break;
+                
+            case 2:
+                self.view.backgroundColor = [UIColor greenColor];
+                break;
+                
+        }
+    }
+}
 
 - (void)viewDidLoad
 {
